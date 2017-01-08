e872d56983b7829e31cd0dc2878e6da269ec5e57
joymo <joymo@DESKTOP-N7IGI6V>
2017-01-08 10:39:10 -0500

    Added function export sample

--- ECMA5/entry.js
+++ ECMA5/entry.js
@@ -1,3 +1,6 @@
-document.write("God it love");
+document.write("Webpack working");
 var content = require("./content")
 document.write(content);
+
+var logger = require("./logger");
+logger("Logger worked");
