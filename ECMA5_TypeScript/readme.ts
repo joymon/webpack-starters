c89b43d061d37f67b421b554f460a9dd6fd4ebb2
Joy George Kunjikkuru <joymon@gmail.com>
2017-01-08 10:10:57 -0500

    Create readme.ts

---
+++ ECMA5_TypeScript/readme.ts
@@ -0,0 +1,10 @@
+#ECMA5 JavaScript and TypeScript
+
+- ES5 and TS code interacting each other via modules.
+- Transpiling of TS, and bundling.
+- Used @types/node typing for requre.
+- No dev server or watch mode.
+
+#How to run
+- Run command webpack. It will produce bundle.js
+- Open the index.html in browser.
